0295cb9a9015a3d1d4a4ff3c5d17cdb452c9f433
Marta <neurotecnoedu@gmail.com>
2022-07-22 13:53:23 +0200

    feat: filtramos assets que empiezan por EUR

--- llamada.py
+++ llamada.py
@@ -5,16 +5,20 @@ cabeceras = {
     'X-CoinAPI-Key': apikey}   #3º. me genero diccionario cabeceras con codigo proporcionado + apikey
 
 #4º. llamo a todos los datos que necesito: lo tengo en documentación CoinAPI.io en Market Dta - Rest API y en Metadata
-api_url = 'http://rest-sandbox.coinapi.io' #url base (hay que quitar barra última!)
-endpoint = '/v1/exchanges'  #ruta
+api_url = 'http://rest.coinapi.io' #url base (hay que quitar barra última y tb he quitado sandobox de pruebas)
+endpoint = '/v1/assets'  #ruta
 
 url = api_url + endpoint    #5º. genero url definitiva a la que quiero llamar
 respuesta = requests.get(url, headers=cabeceras)  #6º. hago petición get (librería) y luego me guardaré respuesta
 codigo = respuesta.status_code  #7º. consulto qué código me devuelve de entre los existentes standard
 
 if codigo ==200:    #8º. si el codigo a ido bien imprimo este resultado
-    print ('el resultado de la consulta es:')
-    print (respuesta.text)  #la respuesta me viene dada en formato texto 
+    print ('las monedas disponibles son:')
+    respuesta_json = respuesta.json ()  #recibo json interpretable en python para convertir objeto en dicc. y poder acceder a propiedades de la moneda
+    for moneda in respuesta_json:   #para cada moneda en respuesta_json puedo acceder a pedir info. de web CoinAPI.io
+        if moneda ['asset_id'].startswith('EUR'):
+            print (moneda['asset_id'], moneda ['name'])
+
 else:   #9º. si el codigo no ha ido bien imprimo este otro resultado 
     print ('la petición a la API ha fallado')
     print (f'codigo de error{codigo}')  #para que me imprima el codigo de la respuesta
