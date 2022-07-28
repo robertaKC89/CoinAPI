f9ffdea3b4590128390c117da4f6062589689e13
Marta <neurotecnoedu@gmail.com>
2022-07-28 09:54:17 +0200

    feat: probando tkinter

--- linea_comandos/hellotk.py
+++ linea_comandos/hellotk.py
@@ -1,10 +1,12 @@
-from tkinter import * # importo de módulo librería entero (me salto un poco las normas)
+# importo de módulo librería entero (me salto un poco las normas)
+from tkinter import *
 from tkinter import ttk
 
-root = Tk ()    #para crear la app necesito nodo de entrada e instancio Tk
-etiqueta = Label (root, 'text = hola TKinter') #especificaré cosas que me hagan falta 
-etiqueta.pack () #con el pack la hago visible en ventana
+root = Tk()  # para crear la app necesito nodo de entrada e instancio Tk
+# especificaré cosas que me hagan falta
+etiqueta = Label(root, 'text = hola TKinter')
+etiqueta.pack()  # con el pack la hago visible en ventana
 
-otra = Label (root, 'text = soy otra etiqueta')
-root.mainloop ()    #bucle principal que gestionará eventos y el programador ya no estará pendiente 
-
+otra = Label(root, 'text = soy otra etiqueta')
+# bucle principal que gestionará eventos y el programador ya no estará pendiente
+root.mainloop()
